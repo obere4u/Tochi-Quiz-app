ad818396a00ed1bdae05f6be0f5d8c084cb24a80
Nwosa <obere4u@gmail.com>
2023-04-03 14:48:51 +0100

    Ade a Progress bar

--- main.js
+++ main.js
@@ -227,6 +227,7 @@ const questions = [
   },
 ];
 
+const html = document.querySelector("html");
 const quizContainer = document.querySelector(".quiz-container");
 const question = document.querySelector(".question");
 const optionA = document.querySelector(".optionA");
@@ -238,51 +239,71 @@ const startButton = document.querySelector("#start");
 let timer = document.querySelector(".time");
 let questionNumberDisplay = document.querySelector(".question-number span");
 const answerButtons = document.querySelectorAll(".answer-option");
+const lineValueElement = document.querySelector(".line-value");
 // const submitBtn = document.querySelector("submit-btn");
 // const resultEl = document.querySelector("result");
 let timerCountDown;
-let questionNumber = 1;
+let questionNumber = 0;
 let currentQuestion = {};
 let score = 0;
 let selectedAnswer;
-
 // let questionNumber = document.querySelector('.question-number');
 
 let shuffledQuestion = []; //the question will be shuffled but for now, it is an empty array
 let playerScore = 0; //holds the player score
 let wrongAttempt = 0; //amount of wrong answers picked by player
 let indexNumber = 0; //will be used in displaying next question
+let correctAnswer = 0;
+let wrongAnswer = 0;
+
+//Create HTML elements
+const messageContainer = document.createElement("div");
+messageContainer.classList.add("message-container");
+quizContainer.appendChild(messageContainer);
+const markAnswerIcons = document.querySelectorAll(".select");
+console.log(markAnswerIcons);
+markAnswerIcons.forEach((markAnswer) => {
+  const rightCheckAnswer = document.createElement("button");
+  console.log(rightCheckAnswer);
+  rightCheckAnswer.innerHTML =
+    '<ion-icon name="checkmark-circle-outline"></ion-icon>';
+  rightCheckAnswer.classList.add("right_check-answer");
+  markAnswer.appendChild(rightCheckAnswer);
+
+  const wrongCheckAnswer = document.createElement("button");
+  wrongCheckAnswer.innerHTML =
+    '<ion-icon name="close-circle-outline"></ion-icon>';
+  wrongCheckAnswer.classList.add("wrong_check-answer");
+  markAnswer.appendChild(wrongCheckAnswer);
+}); //This code selects all the select divs using querySelectorAll, and then loops through each one to append the check answer buttons. Note that we are now appending the check answer buttons to the existing select divs rather than creating new mark-answer divs for each button.
+
+
+
+
+
 
 //function that will shuffle the question and store it the shuffled question array
 function handleQuestions() {
-    shuffledQuestion = [];
-    while (shuffledQuestion.length <= 9) {
-        const pickRandomQuestion =
-        questions[Math.floor(Math.random() * questions.length)];
-        if (!shuffledQuestion.includes(pickRandomQuestion)) {
-        shuffledQuestion.push(pickRandomQuestion); //pushes the question the shuffled question
-        }
-    }
-    return shuffledQuestion;
+  shuffledQuestion = [];
+  while (shuffledQuestion.length <= 9) {
+      const pickRandomQuestion =
+      questions[Math.floor(Math.random() * questions.length)];
+      if (!shuffledQuestion.includes(pickRandomQuestion)) {
+      shuffledQuestion.push(pickRandomQuestion); //pushes the question the shuffled question
+      }
+  }
+  return shuffledQuestion;
 }
 
 shuffledQuestion = handleQuestions();
 
 
 //Event Listeners
+document.addEventListener("DOMContentLoaded", html);
 if (startButton) {
     startButton.addEventListener("click", startQuiz);
 } // checks if startButton is null before adding an event listener to it. This will prevent the "Cannot read properties of null" error from occurring
 nextButton.addEventListener("click", nextQuestion);
-// answerButtons.addEventListener("click", answerCheck);
-// if (answerButtons.length > 0) {
-//     answerButtons.forEach((answerButton) => {
-//         answerButton.addEventListener("click", () => {
-//         selectedAnswer = answerButton.id;
-//         console.log(selectedAnswer)
-//         });
-//     });
-// }
 
 //Functions
 
@@ -290,102 +311,186 @@ nextButton.addEventListener("click", nextQuestion);
 function startQuiz(e) {
   e.preventDefault(); //prevents default action of button
   window.location.href = "questions.html"; // makes the html to switch pages
+
+  // Display welcome message
+  
 }
 
 //
-window.onload = function () {
+window.onload =  () => {
   nextQuestion();
-  //   handleQuestions();
+  displayTimer();
+  handleQuestions();
   /* 
-    The window.onload event is triggered when the entire web page (including all images, scripts, and other resources) has finished loading
-    
-    Anonymous function is used here
+  The window.onload event is triggered when the entire web page (including all images, scripts, and other resources) has finished loading
+  
+  Anonymous function is used here
 
-    Inside the window.onload function, we're calling the nextQuestion() function, which is already declared elsewhere in the code. This will ensure that the nextQuestion() function is called as soon as the page finishes loading.
+  Inside the window.onload function, we're calling the nextQuestion() function, which is already declared elsewhere in the code. This will ensure that the nextQuestion() function is called as soon as the page finishes loading.
 
-    This code ensures that the nextQuestion() function is called automatically when the page is reloaded, so that the first quiz question is displayed without the need for the user to click a button or take any other action.
-    */
+  This code ensures that the nextQuestion() function is called automatically when the page is reloaded, so that the first quiz question is displayed without the need for the user to click a button or take any other action.
+  */
+};
+
+const timerDuration = 16;
+const progressBar = document.querySelector(".progress-bar");
+
+const updateProgressBar = () => {
+  const maxWidth = progressBar.parentNode.offsetWidth; //defines a constant maxWidth and assigns it the value of the width of the parent element of the progressBar element. This will be used to calculate the width of the progress bar.
+  let lineValue = (1 - timerCount / timerDuration) * maxWidth;
+  progressBar.style.width = `${lineValue}px`;
+};
+
+const displayTimer = () => {
+  //declares a constant variable called displayTimer that is a function.
+
+  clearInterval(timerCountDown);
+  timerCountDown = setInterval(() => {
+    //declares a variable called timerCountDown that is a function and sets an interval that will execute every 1 second (1000 milliseconds)
+    timerCount--; //decrements the timerCount variable by 1 each second
+    timer.textContent = `${timerCount}s`; //updates the text content of an HTML element with a class of timer to show the current timerCount value in seconds
+    updateProgressBar();
+    if (timerCount > 5) {
+      //checks whether timerCounter is above 5, if yes the code block runs
+      timer.textContent = `${timerCount}s`;
+      timer.style.color = "green";
+    } else if (timerCount > 0 && timerCount <= 5) {
+      //checks if the timerCount is strictly 5, if yes then from 5 and below will in red color
+      timer.textContent = `${timerCount}s`;
+      timer.style.color = "red";
+    } else {
+      clearInterval(timerCountDown); // if timer reaches 0, clear the interval
+      nextQuestion(); // move to next question
+    }
+  }, 1000);
 };
 
 //function that display next question in the array
 function nextQuestion(e) {
-    if (e) {
-        e.preventDefault(); // preventDefault if the e or event object is defined
-    }
+  if (e) {
+    e.preventDefault(); // preventDefault if the e or event object is defined
+  }
+  messageContainer.innerText = "";
+  handleQuestions(); //calls the handleQuestions() function
+  displayTimer();
+  const currentQuizQuestion = shuffledQuestion[indexNumber]; //sets a new variable called currentQuizQuestion. This variable gets its value from an array called questions, at a position determined by another variable called
 
-    handleQuestions(); //calls the handleQuestions() function
-    displayTimer();
-    let currentQuizQuestion = shuffledQuestion[indexNumber]; //sets a new variable called currentQuizQuestion. This variable gets its value from an array called questions, at a position determined by another variable called
-    const currentQuizQuestionAns = currentQuizQuestion.correctOption;
-    console.log(currentQuizQuestionAns);
-    indexNumber++;; //increments currentQuizQuestion counter by 1. This is very CRUCIAL as it allows us to show the next question in the quiz each time the nextQuestion button is clicked/pressed.
-    question.textContent = currentQuizQuestion.question;
-    optionA.textContent = currentQuizQuestion.optionA;
-    optionB.textContent = currentQuizQuestion.optionB;
-    optionC.textContent = currentQuizQuestion.optionC;
-    optionD.textContent = currentQuizQuestion.optionD;
-    /* 
-        Sets the contents of the various HTML elements in the quiz. By changing the the textContent of those elements, we will be able to make the question and answer choice visible to the user.
+  question.textContent = currentQuizQuestion.question;
+  optionA.innerHTML = currentQuizQuestion["optionA"];
+  optionB.innerHTML = currentQuizQuestion["optionB"];
+  optionC.innerHTML = currentQuizQuestion["optionC"];
+  optionD.innerHTML = currentQuizQuestion["optionD"];
+  
+  /* 
+    Sets the contents of the various HTML elements in the quiz. By changing the the textContent of those elements, we will be able to make the question and answer choice visible to the user.
 
-        textContent is different from innerHTML
-    */
-    questionNumber = indexNumber; //questionNum
-    questionNumberDisplay.textContent = questionNumber;
-    // if (questionNumber === 10) {
-    //   clearInterval(timerCountDown);
-    // }
-  if (questionNumber === 11) {
+    textContent is different from innerHTML
+  */
+  questionNumber = indexNumber + 1; //questionNumber will now be 1 because indexNumber is 0
+  questionNumberDisplay.textContent = questionNumber;
+  // if (questionNumber === 10) {
+  //   clearInterval(timerCountDown);
+  // }
+  if (indexNumber === 11) {
     clearInterval(timerCountDown);
     window.location.href = "end.html";
   } else {
     timerCount = 16; // sets the value for variable timeCount to 15
     clearInterval(timerCountDown); // clears the timerCountDown function once the time is up
     displayTimer(); // call the displayTimer function and starts the time for next question
+    answerCheck();
   }
+  indexNumber++; //increments currentQuizQuestion counter by 1. This is very CRUCIAL as it allows us to show the next question in the quiz each time the nextQuestion button is clicked/pressed.
+  return currentQuizQuestion;
 }
-
-
-
-const displayTimer = () => {
-    //declares a constant variable called displayTimer that is a function.
-    let timerCount = 16;
-    clearInterval(timerCountDown);
-    timerCountDown = setInterval(() => {
-    //declares a variable called timerCountDown that is a function and sets an interval that will execute every 1 second (1000 milliseconds)
-    timerCount--; //decrements the timerCount variable by 1 each second
-    timer.textContent = `${timerCount}s`; //updates the text content of an HTML element with a class of timer to show the current timerCount value in seconds
-    if (timerCount > 5) {
-        //checks whether timerCounter is above 5, if yes the code block runs
-        timer.textContent = `${timerCount}s`;
-        timer.style.color = "green";
-    } else if (timerCount > 0 && timerCount <= 5) {
-        //checks if the timerCount is strictly 5, if yes then from 5 and below will in red color
-        timer.textContent = `${timerCount}s`;
-        timer.style.color = "red";
-    } else {
-        clearInterval(timerCountDown); // if timer reaches 0, clear the interval
-        nextQuestion(); // move to next question
-    }
-  }, 1000);
-};
-
-
-
 
 // Checks for answers
 function answerCheck() {
+  // Get the current quiz question and its answer
+  const currentQuizQuestion = shuffledQuestion[indexNumber];
+  const currentQuizQuestionAnswer = currentQuizQuestion.correctOption.trim();
+
+  // Loop through each answer button and add a click event listener
+  answerButtons.forEach((answerButton) => {
+    // Remove the previous click event listener
+    answerButton.removeEventListener("click", handleAnswerClick);
+
+    // Add a new click event listener
+    answerButton.addEventListener("click", handleAnswerClick);
+  });
+
+  function handleAnswerClick() {
+    // Get the selected answer and the correct answer for the current question
+    const selectedAnswer = this.dataset.answer;
+    const trimmedSelectedAnswer = selectedAnswer.trim();
+    const trimmedCorrectAnswer = currentQuizQuestionAnswer;
+
+    // Check if the selected answer is correct or not
+    if (trimmedSelectedAnswer === trimmedCorrectAnswer) {
+      // If the selected answer is correct, increase the score and go to the next question
+      correctAnswer++;
+      showMessage("correct!", true);
+      this.style.outline = "1px solid green";
+      clearInterval(timerCountDown);
+      
+      // Increase the question number
+      indexNumber++;
+      questionNumber++;
+    } else {
+      // If the selected answer is wrong, increase the wrong answer count
+      wrongAnswer++;
+      showMessage("wrong!", false);
+      // Increase the question number
+      indexNumber++;
+      questionNumber++;
+      // indexNumber++;
+    }
+
+    
+
+    // Check if all 10 questions have been answered
+    if (indexNumber === 11) {
+      clearInterval(timerCountDown);
+      window.location.href = "end.html"
+    } else {
+      // If not, move on to the next question and reset the timer
+      handleQuestions();
+      timerCount = 16;
+      clearInterval(timerCountDown);
+      displayTimer();
+    }
+  }
 
   
+
 }
 
+//remember to add the HTML
+function showMessage(message, isCorrect) {
+  console.log("Showing message:", message);
+  messageContainer.innerText = message;
+  messageContainer.classList.remove("message-correct", "message-wrong");
+  if (isCorrect) {
+    messageContainer.classList.add("message-correct");
+  } else {
+    messageContainer.classList.add("message-wrong");
+  }
+}
 
+function checkAnswer(answer) {
+  if (answer === shuffledQuestion[indexNumber]["correct"]) {
+    const markAnswer = document.querySelectorAll(".selected")[indexNumber];
+    const rightCheckAnswer = markAnswer.querySelector(".right_check-answer");
+    rightCheckAnswer.classList.add("checked");
 
+    score++;
+  } else {
+    const markAnswer = document.querySelectorAll(".mark-answer")[indexNumber];
+    const rightCheckAnswer = markAnswer.querySelector(".right_check-answer");
+    const wrongCheckAnswer = markAnswer.querySelector(".wrong_check-answer");
+    rightCheckAnswer.classList.add("checked");
+    wrongCheckAnswer.classList.add("checked");
+  }
 
-
-// function deselectAnswers() {
-//   const answerElements = document.querySelectorAll(".answer-option");
-
-//   answerElements.forEach((answerElement) => {
-//     answerElement.querySelector(".answer-option").checked = false;
-//   });
-// }
+  disableOptions();
+}
